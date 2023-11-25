59234a1903bfbad75004e819364eb7bf65d756d2
raisultan <ki.xbozz@gmail.com>
2023-11-25 15:38:12 +0600

    value.py type annotated, mypy approved ✅

--- micrograd/value.py
+++ micrograd/value.py
@@ -1,101 +1,104 @@
+from __future__ import annotations
 import math
+from typing import Callable
 
 
 class Value:
     """Representation of singular scalar value with its gradient."""
 
-    def __init__(self, data, _children = (), _op = '', label=''):
+    def __init__(self, data: float, _children: tuple[Value, ...] = (), _op: str = '', label: str = ''):
         self.data = data
         self._prev = set(_children)
         self._op = _op
-        self._backward = lambda: None
+        self._backward: Callable[[], None] = lambda: None
 
         self.label = label
         # derivative of the output with respect to current value, yet no effect
         self.grad = 0.0
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f'Value(data={self.data})'
 
-    def __add__(self, other):
+    def __add__(self, other: Value | float) -> Value:
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, _children=(self, other), _op='+')
 
-        def _backward(): self.grad += 1 * out.grad; other.grad += 1 * out.grad
+        def _backward() -> None: self.grad += 1 * out.grad; other.grad += 1 * out.grad
 
         out._backward = _backward
         return out
 
-    def __radd__(self, other):
+    def __radd__(self, other: Value) -> Value:
         return self + other
 
-    def __mul__(self, other):
+    def __mul__(self, other: Value | float) -> Value:
         other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, _children=(self, other), _op='*')
 
-        def _backward(): self.grad += out.grad * other.data; other.grad += out.grad * self.data
+        def _backward() -> None: self.grad += out.grad * other.data; other.grad += out.grad * self.data
 
         out._backward = _backward
         return out
 
-    def __rmul__(self, other):
+    def __rmul__(self, other: Value) -> Value:
         return self * other
 
-    def __pow__(self, other):
+    def __pow__(self, other: Value | int) -> Value:
         assert isinstance(other, (int, float)), "Only supporting int/float powers for now"
         out = Value(self.data**other, (self,), f'**{other}')
 
-        def _backward(): self.grad += other * self.data**(other-1) * out.grad
-        out._backward = _backward
+        def _backward() -> None: self.grad += other * self.data**(other-1) * out.grad
 
+        out._backward = _backward
         return out
 
-    def __neg__(self):
+    def __neg__(self) -> Value:
         return self * -1
 
-    def __sub__(self, other):
+    def __sub__(self, other: Value) -> Value:
         return self + (-other)
 
-    def __rsub__(self, other):
+    def __rsub__(self, other: Value) -> Value:
         return other + (-self)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: Value) -> Value:
         return self * other**-1
 
-    def __rtruediv__(self, other):
+    def __rtruediv__(self, other: Value) -> Value:
         return other * self**-1
 
-    def tanh(self):
+    def tanh(self) -> Value:
         x = self.data
         t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
         out = Value(t, _children=(self, ), _op='tanh')
 
-        def _backward(): self.grad += (1 - t**2) * out.grad
+        def _backward() -> None: self.grad += (1 - t**2) * out.grad
 
         out._backward = _backward
         return out
 
-    def exp(self):
+    def exp(self) -> Value:
         exp_x = math.exp(self.data)
         out = Value(exp_x, (self,), 'exp')
 
-        def _backward(): self.grad += exp_x * out.grad
+        def _backward() -> None: self.grad += exp_x * out.grad
         out._backward = _backward
 
         return out
 
-    def backward(self):
+    def backward(self) -> None:
+        """Runs backward pass."""
         topo = []
         visited = set()
 
-        def build_topo(node):
+        def _build_topo(node: Value) -> None:
             if node not in visited:
                 visited.add(node)
                 for child in node._prev:
-                    build_topo(child)
+                    _build_topo(child)
                 topo.append(node)
 
-        build_topo(self)
+        _build_topo(self)
 
         self.grad = 1.0
         for node in reversed(topo):
